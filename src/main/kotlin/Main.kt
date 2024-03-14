c7d8f8ecca40414401aa4f5ee3cf0bfa344cda9d
Roman <mih-xrom@ya.ru>
2024-03-14 20:21:33 +0300

    mainmenu add list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import achives.Archive
 import menu.*
 
 fun main(args: Array<String>) {
-    var archives: List<Archive> = listOf()
+    var archives: MutableList<Archive> = listOf()
     val firstScreen = DisplayMenu(Screens.SELECT_ARCH, MainMenu(archives))
     firstScreen.printMenu()
 }
--- src/main/kotlin/menu/MainMenu.kt
+++ src/main/kotlin/menu/MainMenu.kt
@@ -5,7 +5,11 @@ import achives.Archive
 class MainMenu(private val listOfArchive: List<Archive>): Menu {
     override fun getHeadOfMenu(): String = "Список архивов:"
     override fun getMenuItems(): List<String> {
-        return listOf("Создать архив", listOfArchive.toString(), "Выход")
+        var listOfArch: MutableList<String> = listOf()
+        if (!listOfArchive.isEmpty())
+            for (arch in listOfArchive)
+                listOfArch.add(arch.toString())
+        return listOf("Создать архив", listOfArch , "Выход")
     }
 
 }
